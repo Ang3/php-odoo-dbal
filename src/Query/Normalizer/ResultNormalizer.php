7eae0babfa7081f6b87712e08148c0b78fe2c1f8
Ang3 <Ang3@users.noreply.github.com>
2024-01-17 16:21:25 +0000

    Fix PHP code via php-cs-fixer (auto) [no ci]

--- src/Query/Normalizer/ResultNormalizer.php
+++ src/Query/Normalizer/ResultNormalizer.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of package ang3/php-odoo-dbal
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Ang3\Component\Odoo\DBAL\Query\Normalizer;
 
 use Ang3\Component\Odoo\DBAL\Schema\Metadata\ModelMetadata;
@@ -7,22 +16,22 @@ use Ang3\Component\Odoo\DBAL\Types\TypeConverterInterface;
 
 class ResultNormalizer implements ResultNormalizerInterface
 {
-	public function __construct(private readonly TypeConverterInterface $typeConverter)
-	{
-	}
+    public function __construct(private readonly TypeConverterInterface $typeConverter)
+    {
+    }
 
-	public function normalize(ModelMetadata $model, array $payload = [], array $context = []): array
-	{
-		foreach ($payload as $fieldName => $value) {
-			$field = $model->getField($fieldName);
+    public function normalize(ModelMetadata $model, array $payload = [], array $context = []): array
+    {
+        foreach ($payload as $fieldName => $value) {
+            $field = $model->getField($fieldName);
 
-			if ($field->isAssociation() || null === $value) {
-				continue;
-			}
+            if ($field->isAssociation() || null === $value) {
+                continue;
+            }
 
-			$payload[$fieldName] = $this->typeConverter->convertToPhpValue($value, $field->getType()->value, $context);
-		}
+            $payload[$fieldName] = $this->typeConverter->convertToPhpValue($value, $field->getType()->value, $context);
+        }
 
-		return $payload;
-	}
+        return $payload;
+    }
 }
